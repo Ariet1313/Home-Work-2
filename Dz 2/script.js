80abfcd5396d83c0c28f2e9a794121a82641c778
Ariet1313 <novyjpolzovatel211@gmail.com>
2024-05-28 16:53:58 +0600

    Add files via upload

---
+++ Dz 2/script.js
@@ -0,0 +1,14 @@
+const First = parseInt(prompt('Введите первое число'))
+
+const Second  = parseInt(prompt('Введите второе число'));
+
+// if(First=isNaN, Second=isNaN 
+//     {console.log("Ошибка вы ввели не число")}
+// )
+
+
+for (let i = First; i < Second; i++) {
+        const element = i;
+        console.log(i)
+        
+    }
